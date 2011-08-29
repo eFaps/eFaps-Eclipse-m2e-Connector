470b53f49087297192a15d68079e82085aed21e3
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-08-29 14:50:13 +0000

    - update of m2e connector to add automatically to the classpath

--- src/org/efaps/eclipse/m2e/connector/EFapsProjectConfigurator.java
+++ src/org/efaps/eclipse/m2e/connector/EFapsProjectConfigurator.java
@@ -18,14 +18,22 @@
  * Last Changed By: $Author$
  */
 
-
 package org.efaps.eclipse.m2e.connector;
 
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecution;
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+import org.eclipse.m2e.jdt.IClasspathDescriptor;
 
 /**
  * TODO comment!
@@ -36,11 +44,38 @@ import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
 public class EFapsProjectConfigurator
     extends AbstractJavaProjectConfigurator
 {
+
     @Override
     public AbstractBuildParticipant getBuildParticipant(final IMavenProjectFacade _projectFacade,
                                                         final MojoExecution _execution,
                                                         final IPluginExecutionMetadata _executionMetadata)
     {
         return new EFapsBuildParticipant(_execution);
+    }
+
+    @Override
+    public void configureRawClasspath(final ProjectConfigurationRequest request,
+                                      final IClasspathDescriptor classpath,
+                                      final IProgressMonitor monitor)
+        throws CoreException
+    {
+        final IProject project = request.getProject();
+        final IMavenProjectFacade facade = request.getMavenProjectFacade();
+        // search for folder that are in the src folder and are named "ESJP" and add them to the classpath
+        final DirectoryScanner ds = new DirectoryScanner();
+        final String[] includes = new String[] { "**src\\**\\ESJP" };
+        final String[] excludes = new String[] { "**\\.svn\\**" };
+        ds.setBasedir(facade.getPomFile().getParent());
+        ds.setIncludes(includes);
+        ds.setExcludes(excludes);
+        ds.setCaseSensitive(true);
+        ds.scan();
+        final String[] dirs = ds.getIncludedDirectories();
+        for (final String dir : dirs) {
+            final Path path = new Path(project.getFullPath().toString() + File.separator + dir);
+            if (!classpath.containsPath(path)) {
+                classpath.addSourceEntry(path, facade.getOutputLocation(), true);
+            }
+        }
     }
 }
